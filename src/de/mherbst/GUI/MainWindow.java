09f1c2f3ba7e722ff3e242df0c231025ffc1318a
Meinolf Herbst <meinolf.herbst@t-online.de>
2021-07-30 23:59:03 +0200

    some tree elements added

--- src/de/mherbst/GUI/MainWindow.java
+++ src/de/mherbst/GUI/MainWindow.java
@@ -1,10 +1,12 @@
 package de.mherbst.GUI;
 
 import java.awt.EventQueue;
+import java.awt.ScrollPane;
 import java.awt.Toolkit;
 
 import javax.swing.JFrame;
 import javax.swing.JPanel;
+import javax.swing.JScrollPane;
 
 import java.awt.Dimension;
 import javax.swing.JSplitPane;
@@ -12,6 +14,7 @@ import java.awt.BorderLayout;
 import java.awt.Color;
 
 import javax.swing.JTree;
+import javax.swing.tree.DefaultMutableTreeNode;
 import javax.swing.JTabbedPane;
 
 public class MainWindow {
@@ -53,8 +56,89 @@ public class MainWindow {
 
 		JSplitPane splitPane = new JSplitPane();
 		
-		JTree tree = new JTree();
-		splitPane.setLeftComponent(tree);
+		
+		//create the root node
+		DefaultMutableTreeNode root = new DefaultMutableTreeNode("Root");
+		
+		//create the child nodes
+		DefaultMutableTreeNode vegetableNode = new DefaultMutableTreeNode("Vegetables");
+		vegetableNode.add(new DefaultMutableTreeNode("Tomate"));
+		vegetableNode.add(new DefaultMutableTreeNode("Paprika"));
+		vegetableNode.add(new DefaultMutableTreeNode("Blumenkohl"));
+		vegetableNode.add(new DefaultMutableTreeNode("Karotte"));
+		vegetableNode.add(new DefaultMutableTreeNode("Porree"));
+		vegetableNode.add(new DefaultMutableTreeNode("Linsen"));
+		vegetableNode.add(new DefaultMutableTreeNode("Bohnen"));
+		vegetableNode.add(new DefaultMutableTreeNode("Kartoffel"));
+		vegetableNode.add(new DefaultMutableTreeNode("Bohnen"));
+		vegetableNode.add(new DefaultMutableTreeNode("Chikoree"));
+		vegetableNode.add(new DefaultMutableTreeNode("Salat"));
+		vegetableNode.add(new DefaultMutableTreeNode("Mais"));
+		vegetableNode.add(new DefaultMutableTreeNode("Rüben"));
+		vegetableNode.add(new DefaultMutableTreeNode("Weißkohl"));
+		vegetableNode.add(new DefaultMutableTreeNode("Minze"));
+		vegetableNode.add(new DefaultMutableTreeNode("Topinambur"));
+		vegetableNode.add(new DefaultMutableTreeNode("Grünkohl"));
+		vegetableNode.add(new DefaultMutableTreeNode("Rote Beete"));
+		vegetableNode.add(new DefaultMutableTreeNode("Blattspinat"));
+		vegetableNode.add(new DefaultMutableTreeNode("Brokkoli"));
+		vegetableNode.add(new DefaultMutableTreeNode("Chicoree"));
+		vegetableNode.add(new DefaultMutableTreeNode("Kohlrabi"));
+		vegetableNode.add(new DefaultMutableTreeNode("Pak Choi"));
+		vegetableNode.add(new DefaultMutableTreeNode("Pastinaken"));
+		vegetableNode.add(new DefaultMutableTreeNode("Sellerie"));
+		vegetableNode.add(new DefaultMutableTreeNode("Wirsing"));
+		vegetableNode.add(new DefaultMutableTreeNode("Kürbis"));
+		vegetableNode.add(new DefaultMutableTreeNode("Petersilie"));
+		
+		
+		DefaultMutableTreeNode fruitNode = new DefaultMutableTreeNode("Fruits");
+		fruitNode.add(new DefaultMutableTreeNode("Banane"));
+		fruitNode.add(new DefaultMutableTreeNode("Orange"));
+		fruitNode.add(new DefaultMutableTreeNode("Erdbere"));
+		fruitNode.add(new DefaultMutableTreeNode("Mango"));
+		fruitNode.add(new DefaultMutableTreeNode("Apfel"));
+		fruitNode.add(new DefaultMutableTreeNode("Birne"));
+		fruitNode.add(new DefaultMutableTreeNode("Himbeere"));
+		fruitNode.add(new DefaultMutableTreeNode("Blaubeere"));
+		fruitNode.add(new DefaultMutableTreeNode("Stachelbeere"));
+		fruitNode.add(new DefaultMutableTreeNode("Kirsche"));
+		fruitNode.add(new DefaultMutableTreeNode("Papaya"));
+		fruitNode.add(new DefaultMutableTreeNode("Pampelmuse"));
+		fruitNode.add(new DefaultMutableTreeNode("Tybeere"));
+		fruitNode.add(new DefaultMutableTreeNode("Litchie"));
+		fruitNode.add(new DefaultMutableTreeNode("Melone"));
+		fruitNode.add(new DefaultMutableTreeNode("Clementine"));
+		fruitNode.add(new DefaultMutableTreeNode("Grapefruit"));
+		fruitNode.add(new DefaultMutableTreeNode("Ananas"));
+		fruitNode.add(new DefaultMutableTreeNode("Aprikosen"));
+		fruitNode.add(new DefaultMutableTreeNode("Heidelbeere"));
+		fruitNode.add(new DefaultMutableTreeNode("Johannisbeere"));
+		fruitNode.add(new DefaultMutableTreeNode("Renekloden"));
+		fruitNode.add(new DefaultMutableTreeNode("Mirabellen"));
+		fruitNode.add(new DefaultMutableTreeNode("Pflaume"));
+		
+		
+		DefaultMutableTreeNode nutsNode = new DefaultMutableTreeNode("Nüsse");
+		nutsNode.add(new DefaultMutableTreeNode("Walnüsse"));
+		nutsNode.add(new DefaultMutableTreeNode("Haselnüsse"));
+		nutsNode.add(new DefaultMutableTreeNode("Erdnüsse"));
+		nutsNode.add(new DefaultMutableTreeNode("Pecanüsse"));
+		nutsNode.add(new DefaultMutableTreeNode("Paranüsse"));
+		nutsNode.add(new DefaultMutableTreeNode("Cashewnüsse"));
+		nutsNode.add(new DefaultMutableTreeNode("Walnüsse"));
+		
+		
+		//add the child nodes to the root node
+		root.add(vegetableNode);
+		root.add(fruitNode);
+		root.add(nutsNode);
+		
+		JTree tree = new JTree(root);
+		//tree.setRootVisible(false);
+		
+		
+		splitPane.setLeftComponent(new JScrollPane(tree));
 		
         JPanel panelRot = new JPanel();
         JPanel panelBlue = new JPanel();
