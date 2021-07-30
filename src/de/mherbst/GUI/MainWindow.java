8403559bcc7f5ef7193cad27d7fc76fc87f7f994
Meinolf Herbst <meinolf.herbst@t-online.de>
2021-07-30 23:19:13 +0200

    simple JTree and JSplitPane added

--- src/de/mherbst/GUI/MainWindow.java
+++ src/de/mherbst/GUI/MainWindow.java
@@ -1,8 +1,18 @@
 package de.mherbst.GUI;
 
 import java.awt.EventQueue;
+import java.awt.Toolkit;
 
 import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+import java.awt.Dimension;
+import javax.swing.JSplitPane;
+import java.awt.BorderLayout;
+import java.awt.Color;
+
+import javax.swing.JTree;
+import javax.swing.JTabbedPane;
 
 public class MainWindow {
 
@@ -35,9 +45,38 @@ public class MainWindow {
 	 * Initialize the contents of the frame.
 	 */
 	private void initialize() {
+
+		// Bildschirmgröße ermitteln
+		Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+
 		frame = new JFrame();
-		frame.setBounds(100, 100, 450, 300);
+
+		JSplitPane splitPane = new JSplitPane();
+		
+		JTree tree = new JTree();
+		splitPane.setLeftComponent(tree);
+		
+        JPanel panelRot = new JPanel();
+        JPanel panelBlue = new JPanel();
+        JPanel panelGreen = new JPanel();
+
+        panelRot.setBackground(Color.RED);
+        panelBlue.setBackground(Color.BLUE);
+        panelGreen.setBackground(Color.GREEN);
+		
+		JTabbedPane tabbedPane = new JTabbedPane(JTabbedPane.TOP, JTabbedPane.SCROLL_TAB_LAYOUT);
+		tabbedPane.addTab("Ich bin rot", panelRot);
+		tabbedPane.addTab("Ich bin blau", panelBlue);
+		tabbedPane.addTab("Ich bin grün", panelGreen);
+		
+		splitPane.setRightComponent(tabbedPane);
+		
+		frame.getContentPane().add(splitPane);
+		
+		frame.setSize(screenSize);
+		frame.setTitle("Learning Java Swing");
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setLocationRelativeTo(null);
 	}
 
 }
